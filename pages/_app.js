8b48ca035faca20777fdd26a332fd20f75844823
Maycon Oliveira <maycon.oliveira@live.com>
2022-10-01 03:00:17 -0300

    Atualização do site

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
-import '../styles/globals.css'
+import '../styles/bootstrap.min.css'
+import '../styles/LineIcons.2.0.css'
+import '../styles/animate.css'
+import '../styles/tiny-slider.css'
+import '../styles/glightbox.min.css'
+import '../styles/main.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
